83058030f31f447f2145796fbfe2e87ba79d2526
Smiteshbhore21 <smiteshbhore23@gmail.com>
2026-01-15 18:52:39 +0530

    URL shortner project done

--- Short-URL/index.js
+++ Short-URL/index.js
@@ -1,21 +1,25 @@
 const express = require("express");
 const path = require("path");
+const cookieParser = require("cookie-parser");
 const URL = require('./models/url');
 const urlRoute = require('./routes/url');
 const staticRoute = require('./routes/staticRouter');
+const userRoute = require("./routes/user");
 const app = express();
 const { connectToMongoDB } = require('./connect');
+const { checkForAuthentication, restrictTo } = require("./middlewares/auth");
 const PORT = 8001;
 
 connectToMongoDB('mongodb://127.0.0.1:27017/short-url')
-    .then(() => console.log('MongoDB Connected')
-    );
+    .then(() => console.log('MongoDB Connected'));
 
 app.set("view engine", 'ejs');
 app.set("views", path.resolve('./views'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(checkForAuthentication)
 
 app.get('/test', async (req, res) => {
     const allUrls = await URL.find({});
@@ -24,7 +28,8 @@ app.get('/test', async (req, res) => {
     });
 });
 
-app.use('/url', urlRoute);
+app.use('/url', restrictTo(['NORMAL', , "ADMIN"]), urlRoute);
+app.use("/user", userRoute);
 app.use('/', staticRoute);
 
 app.get('/url/:shortId', async (req, res) => {
